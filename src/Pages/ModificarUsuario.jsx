339b4a057d859c95d19a05b13b9985211cc3ee4f
David Grilo <davidgrilom@gmail.com>
2023-09-04 18:14:53 +0200

    conflictos pesados

--- src/Pages/ModificarUsuario.jsx
+++ src/Pages/ModificarUsuario.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BotonSimple } from "../Components/BotonSimple"
 import { Forms } from "../Components/Forms"
 import { Input } from "../Components/Input"
@@ -73,54 +72,3 @@ export function ModificarUsuario() {
         
     )
 }
-=======
-import { BotonSimple } from "../Components/BotonSimple";
-import { Forms } from "../Components/Forms";
-import { Input } from "../Components/Input";
-import "../Styles/ModificarUsuario.css";
-import avatar from "../imagenes/avatar.jpg";
-import { useGetLogin } from "../../Hooks/useGetLogin";
-
-export function ModificarUsuario() {
-  // useGetLogin();
-
-  return (
-    <main className="main mod-u">
-      <h2 className="heading2-mod-u">Modificar Perfil</h2>
-      <Forms clase={"form-mod-u"}>
-        <div className="div-form-inp">
-          <Input
-            name={"name"}
-            clase={"input"}
-            type={"text"}
-            label={"Nombre:"}
-            autocomplete={"off"}
-          />
-          <Input
-            name={"password"}
-            clase={"input"}
-            type={"password"}
-            label={"Password:"}
-            autocomplete={"off"}
-          />
-        </div>
-        <div className="div-form-img">
-          <Input
-            name={"avatar"}
-            clase={"hidden"}
-            type={"file"}
-            label={"Imagen de Perfil:"}
-          />
-          <BotonSimple clase={"boton-simple"}>Seleccionar Archivo</BotonSimple>
-          <div className="imagen-perfil">
-            <img className="avatar-form" src={avatar} alt="avatar" />
-          </div>
-        </div>
-      </Forms>
-      <div>
-        <BotonSimple clase={"boton-simple"}>Enviar</BotonSimple>
-      </div>
-    </main>
-  );
-}
->>>>>>> fbce1c79092825f05f1ca5941cfcc82b0598a3ca
