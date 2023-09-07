d221263b88012201536f9a590f494b0a04060d58
Raquel <rmontenemoovecars@gmail.com>
2023-09-08 00:13:50 +0200

    contexto formulario

--- src/Components/Forms.jsx
+++ src/Components/Forms.jsx
@@ -1,11 +1,62 @@
+import { useState } from "react";
 
+export function Forms({ clase, children, onSubmit, schema, initialValue }) {
+  const [formState, setFormState] = useState({
+    isTouched: false,
+    isLoading: false,
+    formValue: initialValue || {},
+  });
 
-export function Forms({clase, children, onSubmit}) {
+  const [, errors] = validate(schema, formState.formValue);
 
+  function updateFormValue(newFormValue) {
+    setFormState((oldFormState) => {
+      return {
+        ...oldFormState,
+        formValue: {
+          ...oldFormState.formValue,
+          ...newFormValue,
+        },
+      };
+    });
+  }
 
-    return(
-        <form onSubmit={onSubmit} className={clase} >
-            {children}
-        </form>
-    )
+  async function onFormSubmit(e) {
+    e.preventDefault();
+
+    setFormState((oldFormState) => {
+      return {
+        ...oldFormState,
+        isTouched: true,
+        isLoading: true,
+      };
+    });
+
+    const [isValid] = validate(schema, formState.formValue);
+
+    if (!isValid) {
+      return setFormState((oldFormState) => {
+        return {
+          ...oldFormState,
+          isTouched: true,
+          isLoading: false,
+        };
+      });
+    }
+    await onSubmit(formState.formValue);
+
+    setFormState({
+      isTouched: false,
+      isLoading: false,
+      formValue: {},
+    });
+  }
+
+  return (
+    <FormContext.Provider value={{ ...formState, errors, updateFormValue }}>
+      <form onSubmit={onFormSubmit} className={clase}>
+        {children}
+      </form>
+    </FormContext.Provider>
+  );
 }
