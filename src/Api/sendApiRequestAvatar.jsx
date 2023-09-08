79c2aa42589f8517f9b25f2adcd6f547a247a55b
Raquel <rmontenemoovecars@gmail.com>
2023-09-08 13:35:46 +0200

    subiendo el archivo registro y foto abatar al backend

---
+++ src/Api/sendApiRequestAvatar.jsx
@@ -0,0 +1,31 @@
+import { API_HOST } from "../../utils/constants";
+import { METHOD } from "../Api/sendApiRequest";
+
+export async function sendApiRequestAvatar(requestObject) {
+  const UPLOAD_URL = API_HOST + `/registro/avatar`;
+
+  const headers = {};
+
+  const fetchPromises = uploadRegistroUsuario((requestObject) => {
+    formData.append(requestObject);
+
+    return fetch(UPLOAD_URL, {
+      method: METHOD.POST,
+      headers,
+      body: formData,
+    });
+  });
+
+  const fetchResponses = await Promise.all(fetchPromises);
+
+  const allOk = fetchResponses.every((response) => response.ok);
+
+  return {
+    success: allOk,
+    ...(!allOk && {
+      error: {
+        msg: "No se ha podido realizar el registro",
+      },
+    }),
+  };
+}
