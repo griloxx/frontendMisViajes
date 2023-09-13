133205e154a6066cee89f29db8f60afebf0bdcde
rakalito <raullaragomez@gmail.com>
2023-09-13 16:28:05 +0200

    boton busqueda

--- src/Pages/ListarEntradas.jsx
+++ src/Pages/ListarEntradas.jsx
@@ -1,17 +1,25 @@
 import { BotonIcono } from "../Components/BotonIcono";
 import { Entrada } from "../Components/Entrada";
+import React, { useState, useEffect } from "react";
 import "../Styles/home.css";
+import { MenuBusqueda } from "../Components/MenuBusqueda";
 
 export function ListarEntradas() {
-  
-
+  const [botonClickeado, setBotonClickeado] = useState(false);
+  const handleClick = () => {
+    botonClickeado ? setBotonClickeado(false) : setBotonClickeado(true);
+  };
   return (
     <main>
-        <>
-          <Entrada />
-          <BotonIcono clase={"boton-busqueda"} icono={"travel_explore"} />
-        </>
-      
+      <>
+        <MenuBusqueda />
+        <Entrada />
+        <BotonIcono
+          clase={"boton-busqueda"}
+          icono={"travel_explore"}
+          onClick={handleClick}
+        />
+      </>
     </main>
   );
 }
