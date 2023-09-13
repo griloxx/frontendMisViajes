ed8d8d8171e5bd6c2e5ebadd1d2f07e7380503e4
David Grilo <davidgrilom@gmail.com>
2023-09-13 19:09:44 +0200

    estilos busqueda

--- src/Pages/ListarEntradas.jsx
+++ src/Pages/ListarEntradas.jsx
@@ -2,18 +2,42 @@ import { BotonIcono } from "../Components/BotonIcono";
 import { Entrada } from "../Components/Entrada";
 import "../Styles/home.css";
 import { MenuBusqueda } from "../Components/MenuBusqueda";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function ListarEntradas() {
-  const [menu, setmenu] = useState(false);
+  const [menu, setMenu] = useState(false);
+  const [display, setDisplay] = useState(false);
 
   function handleClick() {
-    setmenu(!menu);
+    setMenu(!menu);
   }
+
+  useEffect(() => {
+    let prevScroll = window.scrollY;
+    
+    function efectoScroll() {
+      const actualScroll = window.scrollY;
+
+      if (actualScroll < prevScroll) {
+        setDisplay(false);
+      } else {
+        actualScroll > 80 && setDisplay(true);
+      }
+     prevScroll = actualScroll;
+    };
+
+    window.addEventListener("scroll", efectoScroll);
+
+    return () => {
+      window.removeEventListener("scroll", efectoScroll);
+    };
+      
+  }, [])
+  
   return (
     <main>
       <>
-        <MenuBusqueda menu={menu} />
+        <MenuBusqueda display={display} menu={menu} />
         <Entrada />
         <BotonIcono
           clase={"boton-busqueda"}
