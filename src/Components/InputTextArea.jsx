b8f61713264d547d35c25b280580749f3b683d2c
rakalito <raullaragomez@gmail.com>
2023-09-19 02:06:13 +0200

    crearentradaslisto

---
+++ src/Components/InputTextArea.jsx
@@ -0,0 +1,49 @@
+import { useContext, useEffect, useState } from "react";
+import { FormContext } from "../context/FormContext";
+
+export function InputTextArea({ label, name, clase, disabled, maxChars }) {
+  const [isTouched, setIsTouched] = useState(false);
+  const formC = useContext(FormContext);
+
+  function updateRequest(value) {
+    if (!isTouched) {
+      setIsTouched(true);
+    }
+
+    formC.updateFormValue({
+      [name]: value,
+    });
+  }
+  useEffect(() => {
+    if (!formC.isTouched) {
+      setIsTouched(false);
+    }
+  }, [formC.isTouched]);
+
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <div>
+        <textarea
+          disabled={formC.isLoading || disabled}
+          className={clase}
+          id={name}
+          name={name}
+          value={formC.formValue[name] ?? ""}
+          onChange={(e) => updateRequest(e.target.value)}
+        />
+        {maxChars && (
+          <p>
+            {formC.formValue[name]?.length ?? 0} / {maxChars}
+          </p>
+        )}
+        {(formC.isTouched || isTouched) &&
+          formC.errors &&
+          formC.errors[name] &&
+          !formC.isLoading && (
+            <p className="error-form">{formC.errors[name]}</p>
+          )}
+      </div>
+    </>
+  );
+}
